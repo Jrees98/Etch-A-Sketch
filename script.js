6ce6584fa12c0b02ead274d4cec78a12ed1d2e37
Jacob Rees <reescodes@gmail.com>
2023-07-11 21:40:23 -0600

    Add grid button/flexbox

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ for (let i = 0; i < 16 * 16; i++) {
   grid.classList.add('grid');
   containerBox.appendChild(grid);
 }
+
+const grids = document.querySelectorAll('.grid');
+
+grids.forEach((grid) => {
+  grid.addEventListener('mouseover', () => {
+    grid.style.backgroundColor = 'blue'; // Change the color to your desired color
+  });
+});
+
