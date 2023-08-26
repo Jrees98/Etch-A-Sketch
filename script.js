3650f7e18b9bb9ce2a313dd464661c32442ac4c0
Jacob Rees <reescodes@gmail.com>
2023-08-26 12:32:53 -0600

    Finish project

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const mainGrid = document.querySelector('.mainGrid')
 
-for (let i = 1; i<= 256 ; i++) {
-    const div = document.createElement('div')
-    mainGrid.appendChild(div)
-    div.classList.add('gridCell')
+function createGrid(size) {
+    mainGrid.innerHTML = '';
+    
+    for (let i = 1; i <= size * size; i++) {
+        const div = document.createElement('div');
+        mainGrid.appendChild(div);
+        div.classList.add('gridCell');
+    }
+    
+    const cellSizePercentage = 100 / size; // Calculate percentage for cell size
+    const gridCells = document.querySelectorAll('.gridCell');
+    
+    gridCells.forEach(cell => {
+        cell.style.width = cellSizePercentage + '%';
+        cell.style.height = cellSizePercentage + '%';
+    });
 }
+
+
+createGrid(16)
+
 
 const allCells = document.querySelectorAll('.gridCell')
 allCells.forEach((cell) => {
@@ -19,7 +35,7 @@ allCells.forEach((cell) => {
 const resetBtn = document.querySelector('.resetBtn')
 resetBtn.addEventListener('click', () => {
     allCells.forEach(cell => {
-        cell.style.backgroundColor = ''; // Reset to default (empty string)
+        cell.style.backgroundColor = ''; 
     });
 });
 
@@ -30,5 +46,5 @@ const gridSizer = document.querySelector('.sizer')
 
 gridSizer.addEventListener('click', () => {
     let gridInteger = prompt('Enter grid size: ')
-    console.log(gridInteger)
+    createGrid(gridInteger)
 })
